4b6ca53b0fe40f439e6ced140e4f2ce6b9773b76
Stevezhang <zhang.19901990@163.com>
2016-09-09 19:02:22 +0800

    Steve# commit testlib

---
+++ src/main/java/com/tcl/zhanglong/testlib/HelloClass.java
@@ -0,0 +1,12 @@
+package com.tcl.zhanglong.testlib;
+
+/**
+ * Created by Steve on 16/9/9.
+ */
+public class HelloClass {
+
+
+    public void sayHello(){
+        System.out.println("===Hello World.");
+    }
+}
